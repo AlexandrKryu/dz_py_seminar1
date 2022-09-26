aabd1171d4aad0176be6e0e2e1d1a406c3bcc874
AlexandrKryu <a.kryu84@gmail.com>
2022-09-26 21:20:21 +0300

    Короктеровочка

--- task5.py
+++ task5.py
@@ -4,9 +4,9 @@ if quarter == 1:
     print("X > 0 ; Y > 0")
 elif quarter == 2:
     print("X < 0 ; Y > 0")
-elif quarter == 2:
+elif quarter == 3:
     print("X < 0 ; Y < 0")
-elif quarter == 2:
+elif quarter == 4:
     print("X > 0 ; Y < 0")
 else:
     print("для 2х координат не подходит")
